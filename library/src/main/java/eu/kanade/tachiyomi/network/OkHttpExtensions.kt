d86c59ca9ab4c2387116f4c85c05c952e6a8d976
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-02-15 09:33:04 -0300

    Export parseAs function

--- library/src/main/java/eu/kanade/tachiyomi/network/OkHttpExtensions.kt
+++ library/src/main/java/eu/kanade/tachiyomi/network/OkHttpExtensions.kt
@@ -3,9 +3,23 @@ package eu.kanade.tachiyomi.network
 import okhttp3.Call
 import okhttp3.Response
 import rx.Observable
+import kotlin.reflect.KType
+import kotlin.reflect.typeOf
 
 fun Call.asObservable(): Observable<Response> = throw Exception("Stub!")
 
 fun Call.asObservableSuccess(): Observable<Response> = throw Exception("Stub!")
+
+/**
+ * Useful method to parse/serialize JSON responses.
+ * 
+ * @param T A serializable data class.
+ * @return The data class filled with the JSON response data.
+ */
+inline fun <reified T> Response.parseAs(): T {
+    return internalParseAs(typeOf<T>(), this)
+}
+
+fun <T> internalParseAs(type: KType, response: Response): T = throw Exception("Stub!")
 
 suspend fun Call.await(): Response = throw Exception("Stub!")
