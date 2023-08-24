479d3cfe81659b7235f644253fd466e42202924a
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-08-23 23:08:33 -0300

    feat: Add more inline functions to CoroutinesExtensions

--- library/src/main/java/eu/kanade/tachiyomi/util/CoroutinesExtensions.kt
+++ library/src/main/java/eu/kanade/tachiyomi/util/CoroutinesExtensions.kt
@@ -9,3 +9,28 @@ inline fun <A, B> Iterable<A>.parallelMap(crossinline f: suspend (A) -> B): List
     runBlocking {
         map { async(Dispatchers.Default) { f(it) } }.awaitAll()
     }
+
+inline fun <A, B> Iterable<A>.parallelMapNotNull(crossinline f: suspend (A) -> B?): List<B> =
+    runBlocking {
+        map { async(Dispatchers.Default) { f(it) } }.awaitAll().filterNotNull()
+    }
+
+inline fun <A, B> Iterable<A>.parallelMapIndexed(crossinline f: suspend (index: Int, A) -> B): List<B> =
+    runBlocking {
+        mapIndexed { index, it -> async(Dispatchers.Default) { f(index, it) } }.awaitAll()
+    }
+
+inline fun <A, B> Iterable<A>.parallelMapIndexedNotNull(crossinline f: suspend (index: Int, A) -> B?): List<B> =
+    runBlocking {
+        mapIndexed { index, it -> async(Dispatchers.Default) { f(index, it) } }.awaitAll().filterNotNull()
+    }
+
+inline fun <A, B> Iterable<A>.parallelFlatMap(crossinline f: suspend (A) -> Iterable<B>): List<B> =
+    runBlocking {
+        map { async(Dispatchers.Default) { f(it) } }.awaitAll().flatten()
+    }
+
+inline fun <A, B> Iterable<A>.parallelFlatMapIndexed(crossinline f: suspend (index: Int, A) -> Iterable<B>): List<B> =
+    runBlocking {
+        mapIndexed { index, it -> async(Dispatchers.Default) { f(index, it) } }.awaitAll().flatten()
+    }
