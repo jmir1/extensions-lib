ba3f63709e571dd553d3cae021553055da514d48
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-12-16 17:44:52 -0300

    feat: Make coroutine extensions non-blocking

--- library/src/main/java/eu/kanade/tachiyomi/util/CoroutinesExtensions.kt
+++ library/src/main/java/eu/kanade/tachiyomi/util/CoroutinesExtensions.kt
@@ -3,48 +3,48 @@ package eu.kanade.tachiyomi.util
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 
 /**
- * Parallel implementation of [kotlin.collections.Iterable.map].
+ * Parallel implementation of [Iterable.map].
  *
  * @since extensions-lib 14
  */
-inline fun <A, B> Iterable<A>.parallelMap(crossinline f: suspend (A) -> B): List<B> =
-    runBlocking {
-        map { async(Dispatchers.Default) { f(it) } }.awaitAll()
+suspend inline fun <A, B> Iterable<A>.parallelMap(crossinline f: suspend (A) -> B): List<B> =
+    withContext(Dispatchers.IO) {
+        map { async { f(it) } }.awaitAll()
     }
 
 /**
- * Parallel implementation of [kotlin.collections.Iterable.mapNotNull].
+ * Parallel implementation of [Iterable.mapNotNull].
  *
  * @since extensions-lib 14
  */
-inline fun <A, B> Iterable<A>.parallelMapNotNull(crossinline f: suspend (A) -> B?): List<B> =
-    runBlocking {
-        map { async(Dispatchers.Default) { f(it) } }.awaitAll().filterNotNull()
+suspend inline fun <A, B> Iterable<A>.parallelMapNotNull(crossinline f: suspend (A) -> B?): List<B> =
+    withContext(Dispatchers.IO) {
+        map { async { f(it) } }.awaitAll().filterNotNull()
     }
 
 /**
- * Parallel implementation of [kotlin.collections.Iterable.flatMap].
+ * Parallel implementation of [Iterable.flatMap].
  *
  * @since extensions-lib 14
  */
-inline fun <A, B> Iterable<A>.parallelFlatMap(crossinline f: suspend (A) -> Iterable<B>): List<B> =
-    runBlocking {
-        map { async(Dispatchers.Default) { f(it) } }.awaitAll().flatten()
+suspend inline fun <A, B> Iterable<A>.parallelFlatMap(crossinline f: suspend (A) -> Iterable<B>): List<B> =
+    withContext(Dispatchers.IO) {
+        map { async { f(it) } }.awaitAll().flatten()
     }
 
 /**
- * Parallel implementation of [kotlin.collections.Iterable.flatMap], but running
+ * Parallel implementation of [Iterable.flatMap], but running
  * the transformation function inside a [runCatching] block.
  *
  * @since extensions-lib 14
  */
-inline fun <A, B> Iterable<A>.parallelCatchingFlatMap(crossinline f: suspend (A) -> Iterable<B>): List<B> =
-    runBlocking {
+suspend inline fun <A, B> Iterable<A>.parallelCatchingFlatMap(crossinline f: suspend (A) -> Iterable<B>): List<B> =
+    withContext(Dispatchers.IO) {
         map {
-            async(Dispatchers.Default) {
+            async {
                 runCatching { f(it) }.getOrElse { emptyList() }
             }
         }.awaitAll().flatten()
